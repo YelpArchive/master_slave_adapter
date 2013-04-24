28077f048e5b7f0bed2897119e7a76155ae11bd4
Maksym Melnychok <maksym.melnychok@gmail.com>
2013-04-24 16:19:35 +0200

    test ree and 2.0.0
    also bit more verbose

--- spec/all.sh
+++ spec/all.sh
@@ -2,12 +2,20 @@
 
 source `which rvm | sed 's/rvm\/bin/rvm\/scripts/'`
 
-for ruby in 1.8.7 1.9.2 1.9.3; do
+for ruby in 1.8.7 1.9.2 1.9.3 ree 2.0.0; do
   rvm use $ruby
+
   for gemfile in spec/gemfiles/*; do
     if [[ "$gemfile" =~ \.lock ]]; then
       continue
     fi
+
+    # skip unsupported combinations
+    if [[ "$ruby" == "2.0.0" && "$gemfile" =~ "2.3" ]]; then
+      continue
+    fi
+
+    echo "Testing $ruby @ $gemfile"
 
     BUNDLE_GEMFILE=$gemfile bundle install --quiet
     BUNDLE_GEMFILE=$gemfile bundle exec rake spec
